707dbbe3691060723df64447079dd2cfc36431c1
raidenkhan <96030339+raidenkhan@users.noreply.github.com>
2023-09-25 22:31:14 +0000

    Update tower_of_hanoi.c

--- misc/tower_of_hanoi.c
+++ misc/tower_of_hanoi.c
@@ -3,13 +3,14 @@
 #include <stdlib.h>
 
 // Function for Tower of Hanoi algorithm
+//this algorithm solves tower of hanoi problem by making a recursive call
 void hanoi(int noOfDisks, char where, char to, char extra)
 {
     if (noOfDisks != 0)
     {
-        hanoi(noOfDisks - 1, where, extra, to);
+        hanoi(noOfDisks - 1, where, extra, to);//make the first recursive call until the first tower is empty
         printf("Move disk : %d from %c to %c\n", noOfDisks, where, to);
-        hanoi(noOfDisks - 1, extra, to, where);
+        hanoi(noOfDisks - 1, extra, to, where);//make the next recursive call to empty the next tower as well
     }
 }
 int main(void)
